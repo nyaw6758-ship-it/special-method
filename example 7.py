d151403944dbbbd066acf2c0e75f45f77f5a84ac
nyaw6758-ship-it <nyaw6758@gmail.com>
2026-06-30 20:10:27 +0000

    Add files via upload

---
+++ example 7.py
@@ -0,0 +1,24 @@
+#sequential control structure
+
+
+num1=1.5
+num2=6.3
+sum=float(num1)+float(num2)
+print('the sum is =',sum)
+
+    #example 2
+age=18
+if(age>=18):
+    print("elegible for voting")
+else:
+    print("not elegible for voting")
+    print("statement after if sttement")
+
+    #example 3
+age=27
+if (age>=62):
+    print("senior discount")
+elif (18<= age<62):
+    print("no discount")
+else:
+    print("junior discount")
